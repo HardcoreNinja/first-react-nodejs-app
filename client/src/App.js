f1c65b39775e8030db9b6fc4671e18680cac065f
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-12-24 12:23:47 -0500

    Got frontend submitting to backend!
    Got frontend submitting to backend!

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import Axios from 'axios';
 
 export default function App() {
+    const [movieName, setMovieName] = useState('');
+    const [movieReview, setMovieReview] = useState('');
+
+    const submitReview = () => {
+        Axios.post('http://localhost:5000/api/insert',
+            {
+                movieName: movieName,
+                movieReview: movieReview
+            }).then(
+                alert('successful insert!')
+            );
+    };
+
     return (
         <div style={{ textAlign: 'center' }}>
             <h1>CRUD APP</h1>
             <div className='form' style={{ display: 'flex', flexFlow: 'column nowrap', gap: '9px' }}>
                 <label>Movie Name</label>
-                <input type='text' name='movieName' />
+                <input type='text' name='movieName' onChange={(e) => setMovieName(e.target.value)} />
                 <label>Movie Review</label>
-                <input type='text' name='movieReview' />
-                <button>Submit</button>
+                <input type='text' name='movieReview' onChange={(e) => setMovieReview(e.target.value)} />
+                <button onClick={submitReview}>Submit</button>
             </div>
         </div>
     )
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const mysql = require('mysql');
 
@@ -9,11 +11,27 @@ const db = mysql.createPool({
     database: 'crud_database',
 });
 
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES ('movie_1', 'DUDE!');"
-    db.query(sqlInsert, (err, result) => {
-        res.send('Hello Yo');
-        console.log(err);
+// app.get('/', (req, res) => {
+//     const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES ('movie_1', 'DUDE!');"
+//     db.query(sqlInsert, (err, result) => {
+//         res.send('Hello Yo');
+//         console.log(err);
+//     });
+// });
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.post('/api/insert', (req, res) => {
+
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES (?, ?);";
+    db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+        console.log(result);
     });
 });
 
