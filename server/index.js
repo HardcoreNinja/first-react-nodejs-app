d00496503f8007309726851191968c9929e9fbde
Anonymous_PC <anonymous_pc_serbia@protonmail.com>
2022-12-24 11:48:33 -0500

    Got mySQL table updating.
    Got mySQL table updating.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-    res.send('Hello Wold');
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'vcrNfm5cDQ9VT#RHmZbykQ@jA!Jh%WmVF@$Re!KF9FCNaQm6fnenWQ^z4ETR',
+    database: 'crud_database',
 });
 
+app.get('/', (req, res) => {
+    const sqlInsert = "INSERT INTO movie_reviews (movie_name, movie_review) VALUES ('movie_1', 'DUDE!');"
+    db.query(sqlInsert, (err, result) => {
+        res.send('Hello Yo');
+        console.log(err);
+    });
+});
 
 app.listen(5000, () => {
     console.log('Running on Port 5000');
